0a2b6b2e821bb9150bf2d1a6df2f29b04174ea05
cheneyr <cheneyr@si.edu>
2025-04-03 09:55:23 -0400

    updating pdfs for demo
    @jaxinewolfe

--- scripts/renderPDF.R
+++ scripts/renderPDF.R
@@ -5,81 +5,202 @@ library(tidyverse)
 library(rmarkdown)
 library(RefManageR)
 library(readr)
+library(webshot)
+library(mapview)
 tinytex::tlmgr_install("pdfcrop")
 
-        #test render for belize 
-        #render("app/country_insights.Rmd")
 
-## 1. create list of countries included ####
-allstocks <- read_csv("app/data/all_stocks_table.csv")
+## .. 1. Run functions in environment 
 
-            # countries <- allstocks %>% select(country) %>% 
-            #   na.omit() %>% distinct()# 148 total countries included 
+#write bibs 
+writeBibs <- function(chosen_geography){
+  
+  citations_geog <- territory_citations %>% filter(territory == chosen_geography) %>% 
+    left_join(geographies_list) %>% 
+    select(-territory) %>% 
+    mutate(bibliography_id = str_replace_all(bibliography_id, "\\.", ""), #fixing misc formatting errors 
+           bibliography_id = str_replace_all(bibliography_id, "\\ ", "_")) %>% 
+    mutate(uniqueID = if_else(is.na(study_id), bibliography_id, paste(bibliography_id, study_id, ecosystem, sep = "_")))
+  
+  citations_geog <- as.BibEntry(citations_geog %>% 
+                                  column_to_rownames("uniqueID"))
+  
+  WriteBib(citations_geog, file = paste0(chosen_geography, ".bib"))
+}
 
-#list of all countries with tier II data in at least 1 habitat 
-any_tierII_data <- allstocks %>% 
-  filter(TierIorII == "Tier II") %>% 
-  select(country) %>% distinct()
+#render maps 
 
-countries_with_NO_tierII_data <- allstocks %>% 
-  filter(!TierIorII == "Tier II") %>% 
-  select(country) %>% distinct()
+#map 1 -> when there is no CCA data
+renderMap1 <- function(chosen_geog){
+  select_map_territory <- rnaturalearth::ne_countries(returnclass = "sf", scale = "medium") %>% 
+    dplyr::select(territory = admin) %>% 
+    dplyr::mutate(territory = recode(territory, "United States of America" = "United States")) %>% 
+    filter(territory == chosen_geog)
+  
+  geographies_list <- allstocks %>% select(country, territory) %>% 
+    distinct()
+  chosen_country <- geographies_list$country
+  
+  map <- leaflet() %>% 
+    # basemap options
+    addTiles(group = "OSM (default)") %>% #not interactive, do not need multiple layers 
+    
+    #add polygon layer for selected territory border 
+    addPolygons(data = select_map_territory, weight = 2)
+  
+  #save as png to use in md pdf - does not allow leaflet (html widgets)
+  mapshot(map, file = paste0("app/data/reports/", chosen_geog, ".png"))
+  
+}
 
-tier3countries <- allstocks %>% 
-  filter(!is.na(TierIII_mean)) %>% 
-  select(country) %>% distinct()
+#map 2 -> when there is CCA data 
+renderMap2 <- function(chosen_geog){
+  select_map_territory <- rnaturalearth::ne_countries(returnclass = "sf", scale = "medium") %>% 
+    dplyr::select(territory = admin) %>% 
+    dplyr::mutate(territory = recode(territory, "United States of America" = "United States")) %>% 
+    filter(territory == chosen_geog)
+  
+  geographies_list <- allstocks %>% select(country, territory) %>% distinct()
+  
+  chosen_country <- geographies_list %>% filter(territory == chosen_geog) %>% select(country)
+  chosen_country <- as.character(chosen_country)
+  
+  chosen_map_input <- map_input %>% filter(country == chosen_country)
+  
+  map <- leaflet() %>% 
+    # basemap options
+    addTiles(group = "OSM (default)") %>% #not interactive, do not need multiple layers 
+    
+    #add polygon layer for selected territory border 
+    #addPolygons(data = select_map_territory, weight = 2) %>% 
+    addCircleMarkers(data = chosen_map_input, lat = ~latitude, lng = ~longitude)
+  
+  #save as png to use in md pdf - does not allow leaflet (html widgets)
+  mapshot(map, file = paste0("app/data/reports/", chosen_geog, "2.png"))
+  
+}
 
-#if case exists of tier II data, remove from NO data list 
-onlytierI <- anti_join(countries_with_NO_tierII_data, any_tierII_data)
-
-IIIoverlapI <- semi_join(tier3countries, onlytierI) #no countries with tier III values that do not have tier II 
-
-#create lists to loop over 
-tierIcountrylist <- as.list(onlytierI)
-tierIIcountrylist <- as.list(any_tierII_data)
-
-#99 countries have only tier I data --> render "country_insights_NOdata.Rmd"
-#49 countries have any tier II data --> render "country_insights_data.Rmd"
-
-
-## 2. function to render pdfs ####
 
 #when data inclues tier II
-writeCountryInsightsDat <- function(chosen_country){
-
+writeInsightsDat <- function(chosen_geog, bib){
+  
+  bib <-paste0(chosen_geog, ".bib")
   # generate country pdf
-  rmarkdown::render(input = "app/country_insights_data.Rmd",
-                    output_file = paste0(chosen_country, "_Country_Insights.pdf"))
+  rmarkdown::render(input = "app/data/insights_tierII.Rmd",
+                    output_file = paste0(chosen_geog, "_Detailed_Insights.pdf"),
+                    output_dir = "app/data/reports/",
+                    params = list(
+                      country = chosen_geog
+                    ))
 }
-#for some reason it doesn't like printing to the country pdfs folder, moving around manually for now 
-
-      #TEST writeCountryInsights(chosen_country = "United States")
-
 
 #only tier I 
-writeCountryInsightsNOdat <- function(chosen_country){
+writeInsightsNOdat <- function(chosen_geog, bib){
   
+  bib <-paste0(chosen_geog, ".bib")
   # generate country pdf
-  rmarkdown::render(input = "app/country_insights_NOdata.Rmd",
-                    output_file = paste0(chosen_country, "_Country_Insights.pdf"))
+  rmarkdown::render(input = "app/data/insights_NO_tierII.Rmd",
+                    output_file = paste0(chosen_geog,"_Detailed_Insights.pdf"),
+                    output_dir = "app/data/reports/",
+                    params = list(
+                      country = chosen_geog
+                    ))
 }
 
 
+## 2. create list of territories included ####
+app_data <- read_rds("app/data/app_data.rds")
+allstocks <- app_data$main_table
+map_input <- app_data$map_input
+rm(app_data)
 
-## 3. loop over each 'country_list' to render pdfs ####
+#list of all countries with tier II data in at least 1 habitat 
+#update to unique geographies? 
+any_tierII_data <- allstocks %>% 
+  filter(!is.na(soil_TierII_mean)) %>% 
+  select(territory) %>% distinct()
+  #select(country) %>% distinct()
 
-#for countries with only tier I data 
-for (i in seq_along(tierIcountrylist)) {
+geographies_with_NO_tierII_data <- allstocks %>% 
+  filter(is.na(soil_TierII_mean)) %>% 
+  select(territory) %>% distinct()
+#^^ this still contains overlap
+
+tier3geographies <- allstocks %>% 
+  filter(!is.na(soil_TierIII_mean)) %>% 
+  select(territory) %>% distinct()
+
+#if case exists of tier II data, remove from NO data list 
+onlytierI <- anti_join(geographies_with_NO_tierII_data, any_tierII_data)
+
+IIIoverlapI <- semi_join(tier3geographies, onlytierI) #no overlap
+
+## ....create lists to loop over #####
+
+#distinct territories 
+tierIgeographies <- onlytierI  %>% distinct() 
+tierIIgeographies <- any_tierII_data %>% distinct()
+
+#make sure there is no territory overlap 
+check_tier <- semi_join(tierIIgeographies, tierIgeographies) # value should be 0
+
+#create vectors 
+tierIgeog_list <- as.vector(tierIgeographies) %>% unlist(use.names = FALSE)
+tierIIgeog_list <- as.vector(tierIIgeographies) %>% unlist(use.names = FALSE)
+
+#create tier 2 test list 
+tierIITEST <- as.vector(c("Palau", "Ireland", "Vietnam", "Belize", "Australia", "Costa Rica", "South Africa", "El Salvador"))
+tierITEST <- as.vector(c("Cabo Verde", "Peru", "Chile", "Nigeria", "Fiji"))
+
+#### Spot fix citations
+
+#all_citations
+citations <- read_csv("data/citations_by_country.csv")
+    #citations are at country level 
+
+#list of geographies // country, territory 
+geographies_list <- allstocks %>% select(country, territory) %>% distinct()
+
+territory_citations <- citations %>% 
+  mutate(country = case_when(country == "Micronesia"~ "Federated States of Micronesia",
+                             grepl("Russia", country) ~ "Russia",
+                             TRUE ~ country)) %>% 
+  left_join(geographies_list) %>% 
+  select(country, territory, everything())
+
+
+
+##RUN DEMO LOOPS ####
+
+# 1. write bibs
+
+for (i in seq_along(tierIITEST)) {
+
+  writeBibs(tierIITEST[i])
+}
+
+# 2. render maps 
+
+for (i in seq_along(tierIITEST)) {
   
-  {writeCountryInsightsNOdat(tierIcountrylist[i])}
+  {renderMap1(tierIITEST[i])}
 }
 
+# 
+# for (i in seq_along(tierIITEST)) {
+#   
+#   {renderMap2(tierIITEST[i])}
+# }
 
-#for countries with available tier II data 
-for (i in seq_along(tierIIcountrylist)) {
+# 3. render pdfs 
+
+for (i in seq_along(tierITEST)) {
   
-  {writeCountryInsightsDat(tierIIcountrylist[i])}
+  {writeInsightsNOdat(tierITEST[i])}
 }
 
-
+for (i in seq_along(tierIITEST)) {
+  
+  {writeInsightsDat(tierIITEST[i])}
+}
 
